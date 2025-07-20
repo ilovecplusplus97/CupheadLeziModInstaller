a6aca4cb1c25ccb1e30fb6250ccf6ed540cdffcb
ilovecplusplus97 <milovecplusplus97@outlook.com>
2025-07-20 16:26:37 +0800

    修改了Assembly-CSharp.dll文件的哈希值

--- src/constants.h
+++ src/constants.h
@@ -3,4 +3,4 @@
 constexpr std::string_view assemblyFile = "Assembly-CSharp.dll";
 constexpr std::string_view assemblyBackupFile = "Assembly-CSharp-backup.dll";
 constexpr std::string_view assetFile = "lezimod";
-constexpr std::string_view assemblyFileMD5 = "6930132d7e08e58e5b32cf101f5187eb";
+constexpr std::string_view assemblyFileMD5 = "953ea55150c21a146e56529b567a7509";
